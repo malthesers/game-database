1cc043158c17d5985ff94452c677462aa5e9fe2f
Malthe Kusk Lauritsen <malt9830@stud.kea.dk>
2024-02-12 10:11:02 +0100

    Added logo + updated prettierrc

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
@@ -7,16 +8,17 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`
       }}
+      gap='2'
     >
-      <GridItem bg="orange.300" gridArea={'nav'}>
-        Nav
+      <GridItem bg='orange.300' gridArea={'nav'}>
+        <NavBar />
       </GridItem>
-      <Show above="lg">
-        <GridItem bg="pink.300" gridArea={'aside'}>
+      <Show above='lg'>
+        <GridItem bg='pink.300' gridArea={'aside'}>
           Aside
         </GridItem>
       </Show>
-      <GridItem bg="green.300" gridArea={'main'}>
+      <GridItem bg='green.300' gridArea={'main'}>
         Main
       </GridItem>
     </Grid>
