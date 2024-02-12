631847fb1f1706f457fdd51cefcc1db124d69871
Malthe Kusk Lauritsen <malt9830@stud.kea.dk>
2024-02-12 09:36:51 +0100

    Installed prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
 import './App.css'
+import { Grid, GridItem } from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <Grid
+        templateAreas={`"header header"
+                    "nav main"
+                    "nav footer"`}
+        gridTemplateRows={'50px 1fr 30px'}
+        gridTemplateColumns={'150px 1fr'}
+        h="200px"
+        gap="1"
+        color="blackAlpha.700"
+        fontWeight="bold"
+      >
+        <GridItem pl="2" bg="orange.300" area={'nav'}>
+          Header
+        </GridItem>
+        <GridItem pl="2" bg="pink.300" area={'aside'}>
+          Nav
+        </GridItem>
+        <GridItem pl="2" bg="green.300" area={'main'}>
+          Main
+        </GridItem>
+      </Grid>
     </>
   )
 }
