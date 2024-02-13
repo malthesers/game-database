5dba359a91c6877b32a71d42e262267f66f1a359
malt9830 <malt9830@stud.kea.dk>
2024-02-13 19:27:04 +0100

    Added comments for GameGrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -8,11 +8,11 @@ export default function GameGrid() {
   const { games, error } = useGames()
   const { columnCount } = useColumns()
 
+  // Create array of game arrays based on column count
   const gameColumns: Array<Game[]> = Array.from({ length: columnCount }, () => [])
 
-  games.forEach((game, index) => {
-    gameColumns[index % columnCount].push(game)
-  })
+  // Distribute games evenly across columns
+  games.forEach((game, index) => gameColumns[index % columnCount].push(game))
 
   return (
     <>
@@ -20,8 +20,10 @@ export default function GameGrid() {
         <div>error</div>
       ) : (
         <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} gap='4'>
+          {/* For each game array create column */}
           {gameColumns.map((gamesList, index) => (
             <VStack key={index} gap='4'>
+              {/* For each game in game array create game card */}
               {gamesList.map((game) => (
                 <GameCard key={game.id} game={game} />
               ))}
