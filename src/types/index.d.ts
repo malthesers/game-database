a8f0c32046436682869a521bbfc17fb460af2079
Malthe Kusk Lauritsen <malt9830@stud.kea.dk>
2024-02-19 12:06:41 +0100

    Removed unused interfaces

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,13 +1,3 @@
-export interface GameResponse {
-  count: number
-  results: Game[]
-}
-
-export interface GenreResponse {
-  count: number
-  results: Genre[]
-}
-
 export interface Game {
   id: number
   name: string
@@ -33,20 +23,3 @@ export interface ParentPlatform {
     slug: string
   }
 }
-
-// export interface Platform {
-//   platform: PlatformInfo
-//   released_at: string
-//   requirements_en: {
-//     minimum: string
-//     recommended: string
-//   }
-// }
-
-// export interface PlatformInfo {
-//   games_count: number
-//   id: number
-//   image_background: string
-//   name: string
-//   slug: string
-// }
