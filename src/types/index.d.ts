dd399c44d4549ba3f65665ecbf7ce5df21bff65c
Malthe Kusk Lauritsen <malt9830@stud.kea.dk>
2024-02-19 08:40:13 +0100

    Added Genre interface for Game interface

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -7,4 +7,13 @@ export interface Game {
   id: number
   name: string
   background_image: string
+  genres: Genre[]
+}
+
+export interface Genre {
+  games_count: number
+  id: number
+  image_background: string
+  name: string
+  slug: string
 }
