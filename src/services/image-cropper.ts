8d68afac8d816084e88bdfb4c0759bda39fb871c
Malthe Kusk Lauritsen <malt9830@stud.kea.dk>
2024-02-19 10:20:10 +0100

    cropImage method checks parameter type

--- src/services/image-cropper.ts
+++ src/services/image-cropper.ts
@@ -1,3 +1,3 @@
 export default function cropImage(url: string) {
-  return url.replace('/media/', '/media/crop/600/400/')
+  return typeof url === 'string' ? url.replace('/media/', '/media/crop/600/400/') : ''
 }
